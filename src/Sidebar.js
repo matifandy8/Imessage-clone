fcebe1c7f16e2906a79d845c12aa530788db1193
matifandy <matifandy@gmail.com>
2020-10-22 11:49:39 -0300

    first commit

---
+++ src/Sidebar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Sidebar.css";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      i AM THE SIDEBAR
+      <div className="sidebar__header"></div>
+      <div className="sidebar__chats"></div>
+    </div>
+  );
+}
+
+export default Sidebar;
